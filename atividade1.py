25c8c23c6c68a7fa3a2d31b0d50856fdc33e60bd
merettpvp <marcoantoniorochamerett@gmail.com>
2023-08-11 21:03:16 -0300

    atividade 1

---
+++ atividade1.py
@@ -0,0 +1,11 @@
+nome = input ("Qual seu nome?")
+sobrenome = input ("Qual seu sobrenome?")
+altura = float(input ("Qual sua altura em cm?"))
+peso = float(input ("Qual seu peso?"))
+idade = int(input ("Qual sua idade?"))
+adulto = idade >= 18
+print ("Seu nome é:",idade)
+print ("Seu sobrenome é:",sobrenome)
+print ("Sua altura é:",altura,"cm")
+print ("Seu peso é :",peso,"kg")
+print ("Você é um adulto" if adulto else "Você não é um adulto")
